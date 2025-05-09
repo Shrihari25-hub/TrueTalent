4742adb6b0a338f919240c9bdb8c4581c57baaaa
Shrihari Bhilwadikar <134217786+Shrihari25-hub@users.noreply.github.com>
2025-05-09 12:05:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,1 @@
-module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://Shrihari:KingMessi10@cluster0.bdwqc.mongodb.net/TrueTalent_db?retryWrites=true&w=majority&appName=Cluster0'
-  };
+
